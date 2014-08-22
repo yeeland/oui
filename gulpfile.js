775602d90d7e0aafbe8548c684462628c76d790f
Cheston Lee <cheston@optimizely.com>
2014-08-22 13:34:11 -0700

    added error handling to gulp script so hopefully it will not bail on you when you have a syntax error

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var rjs = require('gulp-requirejs');
 var jshint = require('gulp-jshint');
+var gutil = require('gulp-util');
 
 var paths = {
   'scripts' : ['js/**/*.js', '!out/lego.min.js'],
@@ -11,19 +12,27 @@ var paths = {
 gulp.task('lint:js', function() {
   gulp.src(['js/**/*.js', 'js/*.js', '!js/lib/*.js'])
     .pipe(jshint())
+    .on('error', gutil.log)
+    .on('error', gutil.beep)
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('lint:styles', function() {
-  var complete = '======        SCSS-LINT COMPLETE          =======';
   gulp.src(paths.styles)
   //We use shell here because the scss-lint plugin cannot handle scss-lint errors and explodes
-  .pipe(shell(['scss-lint -c .scss-lint.yml ' + paths.styles.join(' ')])).on('error', function() { console.log(complete); process.exit(); });
+  .pipe(shell(['scss-lint -c .scss-lint.yml ' + paths.styles.join(' ')]))
+  .on('error', function() {
+    gutil.log('======        SCSS-LINT COMPLETE          =======', gutil.colors.cyan('123'));
+    gutil.beep();
+    process.exit();
+  });
 });
 
 gulp.task('build:styles', function() {
   gulp.src('scss/lego.scss')
-    .pipe(shell(['sass --compass scss/lego.scss:out/lego.css']));
+    .pipe(shell(['sass --compass scss/lego.scss:out/lego.css']))
+    .on('error', gutil.log)
+    .on('error', gutil.beep);
 });
 
 gulp.task('build:js', function() {
@@ -40,7 +49,10 @@ gulp.task('build:js', function() {
     insertRequire: ['main.js'],
     name: 'lib/almond',
     wrap: true
-  }).pipe(gulp.dest('./out/'));
+  })
+  .on('error', gutil.log)
+  .on('error', gutil.beep)
+  .pipe(gulp.dest('./out/'));
 });
 
 gulp.task('watch', function() {
