60c06c290653c4f2e9c0bb7d71ce2790e85c3952
Tom Genoni <tom.genoni@optimizely.com>
2014-10-28 16:42:17 -0700

    Adding disclosure component.

--- src/js/app/directives.js
+++ src/js/app/directives.js
@@ -2,6 +2,7 @@ define(function(require) {
   return {
     'accordion' : require('app/directives/accordion'),
     'dropdown' : require('app/directives/dropdown'),
+    'disclose' : require('app/directives/disclose'),
     'poptip' : require('app/directives/poptip'),
     'tabs' : require('app/directives/tabs'),
     'editable-text' : require('app/directives/editable_text')
---
+++ src/js/app/directives/disclose.js
@@ -0,0 +1,22 @@
+/**
+ * Directive for simple disclosures
+ *
+ * @author Tom Genoni
+ */
+define(function() {
+  var discloseService = require('app/services/disclose');
+
+  return {
+    data: {
+      show: true
+    },
+    bind: function() {
+      var $el = $(this.el);
+
+      $el.find('> li > a').on('click', function(e) {
+        e.preventDefault();
+        discloseService.activate(this.el, e.target);
+      }.bind(this));
+    }
+  };
+});
