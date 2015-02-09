9d1549611f00338fcee8a40f25ba58882b8ad39d
Tom Genoni <tom.genoni@optimizely.com>
2015-02-09 15:53:57 -0800

    Linter working with one exclude.

--- src/js/common.js
+++
@@ -1,8 +0,0 @@
-require.config({
-  baseUrl: '.',
-  paths: {
-    'jquery' : 'lib/jquery.min',
-    'lodash' : 'lib/lodash.min',
-    'vue' : 'lib/vue.min'
-  }
-});
--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-define(function(require) {
-  var $ = require('jquery');
-
-  $(document).ready(function() {
-    var app = require('app');
-    var appConfig = require('app_config');
-    app.run(appConfig);
-  });
-});
