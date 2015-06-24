ff5aed62b33bf0c5e3170165392749e3a8e6d91b
Daniel O'Connor <daniel@optimizely.com>
2015-06-24 13:20:06 -0700

    Add versioning infrastructure to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
-var compass     = require('gulp-compass'),
+var bump        = require('gulp-bump'),
+    compass     = require('gulp-compass'),
+    filter      = require('gulp-filter'),
+    git         = require('gulp-git'),
     gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     notify      = require('gulp-notify'),
@@ -6,6 +9,7 @@ var compass     = require('gulp-compass'),
     scsslint    = require('gulp-scss-lint'),
     symlink     = require('gulp-symlink'),
     path        = require("path");
+    tagVersion  = require('gulp-tag-version');
 
 var paths = {
   // Limiting linter to first-part directories.
@@ -27,6 +31,34 @@ function swallowError(error) {
 function reportError(error) {
   notify.onError().apply(this, arguments);
   this.emit('end');
+}
+
+// Bumping version number and tagging the repository with it.
+// Please read http://semver.org/
+//
+// https://www.npmjs.com/package/gulp-tag-version
+//
+// You can use the commands
+//
+//     gulp patch     # makes v0.1.0 → v0.1.1
+//     gulp feature   # makes v0.1.1 → v0.2.0
+//     gulp release   # makes v0.2.1 → v1.0.0
+//
+// To bump the version numbers accordingly after you did a patch,
+// introduced a feature or made a backwards-incompatible release.
+function increaseVersion(importance) {
+  // Get all the files to bump version in
+  return gulp.src(['./package.json'])
+    // Bump the version number in those files
+    .pipe(bump({type: importance}))
+    // Save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // Commit the changed version number
+    .pipe(git.commit('New LEGO ' + importance + '.'))
+    // Read only one file to get the version number
+    .pipe(filter('package.json'))
+    // Tag it in the repository
+    .pipe(tagVersion());
 }
 
 // Creates SVG sprite and demo page.
@@ -55,6 +87,24 @@ gulp.task('hook', function () {
     .pipe(symlink('.git/hooks/pre-commit', {
       force: true
     }));
+});
+
+// Release LEGO patch
+// Bumps version from v0.1.0 to v0.1.1
+gulp.task('patch', function() {
+  return increaseVersion('patch');
+});
+
+// Release LEGO feature
+// Bumps version from v0.1.1 to v0.2.0
+gulp.task('feature', function() {
+  return increaseVersion('minor');
+});
+
+// Release breaking LEGO change
+// Bumps version from v0.2.1 to v1.0.0
+gulp.task('release', function() {
+  return increaseVersion('major');
 });
 
 //  compass: compile sass to css
