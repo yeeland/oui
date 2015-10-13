8977a219f6977b617a8795415b76fabab927490e
Tom Genoni <tom.genoni@optimizely.com>
2015-10-13 13:29:23 -0700

    Fully working, needs refactor.

--- src/js/components/editable.js
+++ src/js/components/editable.js
@@ -1,19 +1,43 @@
+function populateInput(pop, $trigger) {
+  var text = $trigger.text();
+  $(".edit-text__input").focus().val(text);
+}
+
+function saveText() {
+  var text = $(".edit-text__input").val();
+  $(".edit.is-active").text(text);
+  $(".pop--over").animate({opacity: 0}, "fast", function(){
+    $(".pop--over").remove();
+  })
+  $(".edit.is-active").removeClass("is-active");
+}
+
 (function($, window, document) {
 
-  $(document).on( "click", ".edit", function() {
-    $trigger = $(this);
-    var text = $trigger.text();
-    $(".edit-text__input").focus().val(text);
+  $(document).bind("change keyup input", '.edit-text__input', function(e) {
+
+      // Wait for keypress before removing disabled.
+      if ( $(".edit.is-active").length ) {
+        $("[data-edit-text-save]").removeAttr("disabled");
+
+        // If return key is pressed.
+        if (e.which == 13) {
+          e.preventDefault();
+          saveText();
+        }
+      }
+
   });
 
   $(document).on( "click", "[data-edit-text-save]", function() {
-    var text = $(".edit-text__input").val();
-    $(".edit").text(text);
-    $(".pop--over").animate({opacity: 0}, "fast")
+    saveText();
   });
 
   $(document).on( "click", "[data-edit-text-cancel]", function() {
-    $(".pop--over").animate({opacity: 0}, "fast")
+    $(".pop--over").animate({opacity: 0}, "fast", function(){
+      $(".pop--over").remove();
+    })
+    $(".edit.is-active").removeClass("is-active")
   });
 
 }(window.jQuery, window, document));
--- src/js/components/popover.js
+++ src/js/components/popover.js
@@ -1,17 +1,44 @@
 (function($, window, document) {
 
-
   $(document).on( "click", "[data-pop-type='over']", function() {
-    var ARROW_SIZE = 7;
+
+    // Get trigger element.
+    var $trigger = $(this);
 
     // Get the properties of the trigger.
     var trigger = getProps( $(this) );
 
-    // Now get properities (heigh/width/etc) of the `pop` element.
-    var pop = getProps( $(".pop--over") );
+    // Set arrow size.
+    var ARROW_SIZE = 7;
 
-    // Show the poptip.
-    showPop(trigger, pop, ARROW_SIZE);
+    // Get html file name from data attr.
+    var popHTML = trigger.dataAttrs[0].popHtml;
+
+    // If this is editable text.
+    if ( $trigger.hasClass("edit") ) {
+      // Remove any existing text edit popover.
+      $("[data-text-edit]").remove();
+      $(".edit.is-active").removeClass("is-active");
+    }
+
+    $.get( "../src/js/html/" + popHTML + ".html", function(data) {
+
+      // Append popover to the body.
+      $("body").append(data);
+
+      // Now get properities (heigh/width/etc) of the `pop` element.
+      var pop = getProps( $(".pop--over") );
+
+      // Show the poptip.
+      showPop(trigger, pop, ARROW_SIZE);
+
+      // If this is an edit text field popover
+      if ( popHTML == "edit--input" || popHTML == "edit--textarea" ) {
+        $trigger.addClass("is-active");
+        populateInput(pop, $trigger);
+      }
+
+    });
 
   });
 
