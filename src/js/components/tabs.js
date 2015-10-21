206b6e546b7a2ed511d8ef079c923c3c4ba1b463
Tom Genoni <tom.genoni@optimizely.com>
2015-10-21 11:32:14 -0700

    Adding tabs.

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -2,5 +2,24 @@ jQuery.noConflict();
 
 (function($) {
 
+  var TABS_NAV_ITEM = ".tabs-nav__item";
+  var TABS_PANE_ITEM = ".tabs-pane__item";
+  var ACTIVE_CLASS = "is-active";
+
+  $(document).on( "click", TABS_NAV_ITEM, function(e) {
+
+    e.preventDefault();
+    var $parent = $(this).closest(".tabs");
+    var index = $parent.find(TABS_NAV_ITEM).index(this);
+
+    // Hide/show clicked tab.
+    $(TABS_NAV_ITEM).removeClass(ACTIVE_CLASS)
+    $(this).addClass(ACTIVE_CLASS);
+
+    // Hide/show corresponding content pane.
+    var $panes =  $parent.find(".tabs-pane");
+    $panes.find(TABS_PANE_ITEM).removeClass(ACTIVE_CLASS);
+    $panes.find(TABS_PANE_ITEM).eq(index).addClass(ACTIVE_CLASS);
+  })
 
 })( jQuery );
