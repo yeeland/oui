419db236d8b761c9f174190206feeae22e56632a
Tom Genoni <tom.genoni@optimizely.com>
2016-03-09 13:21:55 -0800

    Making popovers remote.

--- src/js/components/popover.js
+++ src/js/components/popover.js
@@ -3,72 +3,78 @@
   $(document).on( "click", "[data-oui-pop-type='pop--over']", function(e) {
 
     var ARROW_SIZE = 7;
-    var ACTIVE_POP_ID = "data-oui-active-pop-id";
+    var POPOVER = "data-oui-popover";
+    var TRIGGER_CLASS = "is-pop-trigger";
 
     // Prevent clicks from going through so the popover can stay visible when clicked.
     e.stopPropagation();
     e.preventDefault();
 
-    // Remove any existing popovers.
-    $("["+ACTIVE_POP_ID+"]").remove();
-
     // Get trigger element.
     var $trigger = $(this);
 
-    // Get the properties of the trigger.
-    var trigger = ouiGetProps( $(this) );
+    if ( !$trigger.hasClass(TRIGGER_CLASS) ) {
 
-    // Get html from data attr.
-    var popID = trigger.dataAttrs[0].ouiPopId;
+      // Remove trigger class from open popovers.
+      $("." + TRIGGER_CLASS).removeClass(TRIGGER_CLASS);
 
-    // Reset the classes to remove any existing arrow classes.
-    $("#" + popID).attr("class", "#{OUI_JS_NAMESPACE}pop #{OUI_JS_NAMESPACE}pop--over");
+      // Identify this as the current popover trigger.
+      $trigger.addClass(TRIGGER_CLASS);
 
-    // Clone the target html.
-    var $popHTML = $("#" + popID).clone();
+      // Get the properties of the trigger.
+      var trigger = ouiGetProps( $(this) );
 
-    // Strip the ID, add active ID value to the attr, and append.
-    $popHTML.removeAttr("id").attr(ACTIVE_POP_ID, popID).appendTo("body");
+      // Get html from data attr.
+      var popID = trigger.dataAttrs[0].ouiPopId;
 
-    // If a width is specified via data attr set the width of the popover.
-    // Otherwise it defaults to a max-width specificed in the CSS.
-    var popWidth = trigger.dataAttrs[0].ouiPopWidth;
+      // If a width is specified via data attr set the width of the popover.
+      // Otherwise it defaults to a max-width specificed in the CSS.
+      var popWidth = trigger.dataAttrs[0].ouiPopWidth;
 
-    if ( popWidth !== undefined ) {
-      $("["+ACTIVE_POP_ID+"]").css({
-        width: popWidth,
-        maxWidth: popWidth // Need to set to override default max-width.
-      })
+      // Remove any existing popovers.
+      $("["+POPOVER+"]").remove();
+
+      $.get('popovers/oui-popover-2.html',function(data) {
+
+        $('body').append(data);
+
+        if ( popWidth !== undefined ) {
+          $("["+POPOVER+"]").css({
+            width: popWidth,
+            maxWidth: popWidth // Need to set to override default max-width.
+          })
+        }
+
+        // Get properities (heigh/width/etc) of the `pop` element.
+        var pop = ouiGetProps( $("["+POPOVER+"]") );
+
+        // Show the poptip.
+        ouiShowPop(trigger, pop, ARROW_SIZE);
+
+        $("[data-oui-popover-close]").on('click', function(e) {
+          // Remove trigger class from open popovers.
+          $("." + TRIGGER_CLASS).removeClass(TRIGGER_CLASS);
+          // Remove active popover.
+          $("["+POPOVER+"]").remove();
+        });
+
+        // click.ouiPopover provides a namespace handler that can be unbound
+        // without affecting other handlers attached to the document
+        // http://stackoverflow.com/questions/209029/best-way-to-remove-an-event-handler-in-jquery
+        $(document).bind('click.ouiPopover', function(e) {
+
+          if ( !$(e.target).closest("["+POPOVER+"]").length ) {
+            // Remove trigger class from open popovers.
+            $("." + TRIGGER_CLASS).removeClass(TRIGGER_CLASS);
+            // Remove active popover.
+            $("["+POPOVER+"]").remove();
+
+            $(document).unbind('click.ouiPopover');
+          }
+
+        });
+      });
     }
-
-    // Get properities (heigh/width/etc) of the `pop` element.
-    var pop = ouiGetProps( $("["+ACTIVE_POP_ID+"]") );
-
-    // Show the poptip.
-    ouiShowPop(trigger, pop, ARROW_SIZE);
-
-    $("[data-oui-popover-close]").on('click', function(e) {
-      // Find active popover and remove it.
-      $pop = $(this).closest("[data-oui-popover]");
-      $pop.remove();
-    });
-
-    // click.ouiPopover provides a namespace handler that can be unbound
-    // without affecting other handlers attached to the document
-    // http://stackoverflow.com/questions/209029/best-way-to-remove-an-event-handler-in-jquery
-    $(document).bind('click.ouiPopover', function() {
-
-      // If clicking outside of active pop up hide it, otherwise do nothing.
-      var ACTIVE_POP_ID = "data-oui-active-pop-id";
-
-      if ( !$(e.target).closest("["+ACTIVE_POP_ID+"]").length ) {
-        $("["+ACTIVE_POP_ID+"]").remove()
-      }
-
-      $(document).unbind('click.ouiPopover');
-
-    });
-
   });
 
 })( jQuery );
