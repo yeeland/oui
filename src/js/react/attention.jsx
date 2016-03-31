05471d445575c8938781e894cf5ab983c96c09ce
Daniel O'Connor <daniel@danoc.me>
2016-03-30 18:31:35 -0700

    Turn Attention into a stateless component.

--- src/js/react/attention.jsx
+++ src/js/react/attention.jsx
@@ -2,46 +2,41 @@ import React from 'react';
 
 const maxCharacters = 1000;
 
-export default class Attention extends React.Component {
-  constructor(props) {
-    super(props);
+let renderDismissButton = () => {
+  return (
+    <span className="attention__close">
+      &times;
+    </span>
+  )
+};
+
+let validateChildrenProp = (props, propName, componentName) => {
+  if (!props[propName]) {
+    return new Error('Attention text is required.');
+  } else if (props[propName].length > maxCharacters) {
+    return new Error('Attention text must be under ' + maxCharacters + ' characters.');
   }
+};
 
-  componentDidMount() {}
+ const Attention = ({ alignment, children, isDismissable, type }) => {
+  let colorClassName = type ? 'attention--' + type : '';
+  let alignmentClassName = alignment === 'center' ? 'text--center' : '';
 
-  renderDismissButton() {
-    return (
-      <span className="attention__close">
-        &times;
-      </span>
-    )
-  }
-
-  render() {
-    let isDismissable = this.props.isDismissable;
-    let colorClassName = this.props.type ? 'attention--' + this.props.type : '';
-    let alignmentClassName = this.props.alignment === 'center' ? 'text--center' : '';
-
-    return (
-      <div>
-        <div className={'attention ' +  colorClassName + ' ' + alignmentClassName }>
-          { isDismissable ? this.renderDismissButton() : null }
-          { this.props.children }
-        </div>
+  return (
+    <div>
+      <div className={'attention ' +  colorClassName + ' ' + alignmentClassName }>
+        { isDismissable ? renderDismissButton() : null }
+        { children }
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 Attention.propTypes = {
   alignment: React.PropTypes.oneOf(['left', 'center']),
-  children: (props, propName, componentName) => {
-    if (props[propName].length === 0) {
-      return new Error('Attention text is required.');
-    } else if (props[propName].length > maxCharacters) {
-      return new Error('Attention text must be under ' + maxCharacters + ' characters.');
-    }
-  },
+  children: validateChildrenProp,
   isDismissable: React.PropTypes.bool,
   type: React.PropTypes.oneOf(['bad-news', 'brand', 'good-news', 'warning']),
 };
+
+export default Attention;
