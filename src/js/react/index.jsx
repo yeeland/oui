f7c991fb160aa107895199f9b4c7b3a9e52ca1b6
Daniel O'Connor <daniel@danoc.me>
2016-03-30 18:43:01 -0700

    Create ArrowsInline components.

--- src/js/react/index.jsx
+++ src/js/react/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Attention from './Attention.jsx'
+import ArrowsInline from './ArrowsInline.jsx'
 
 class App extends React.Component {
   render() {
     return (
       <div style={{ margin: '50px auto', width: '800px' }}>
         <h1>OUI React Components</h1>
+
 
         <h2>Attention</h2>
 
@@ -22,6 +24,23 @@ class App extends React.Component {
         <Attention alignment="center">
           Hello! This is a short attention bar.
         </Attention>
+
+
+        <h2>Arrows Inline</h2>
+        <ul>
+          <li>
+            <ArrowsInline direction="up" /> Up
+          </li>
+          <li>
+            <ArrowsInline direction="down" /> Down
+          </li>
+          <li>
+            <ArrowsInline direction="left" /> Left
+          </li>
+          <li>
+            <ArrowsInline direction="right" /> Right
+          </li>
+        </ul>
       </div>
     );
   }
