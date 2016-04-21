38a54da4f68bd8160716d46475adef212c5bfd5a
Daniel O'Connor <daniel@danoc.me>
2016-04-20 23:58:13 -0700

    Add code coverage tooling.

---
+++ src/components/Code/tests/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Code from '../index';
+/* eslint-enable no-unused-vars */
+
+describe('Code', () => {});
