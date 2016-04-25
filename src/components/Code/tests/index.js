1b6676f097b3e0777cd8e9f849017120709ca695
Daniel O'Connor <daniel@danoc.me>
2016-04-25 12:06:38 -0700

    Add tests for code sample.

--- src/components/Code/tests/index.js
+++ src/components/Code/tests/index.js
@@ -1,9 +1,43 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-addons-test-utils';
 import * as testHelpers from '../../../utils/test-helpers';
 import Code from '../index';
-/* eslint-enable no-unused-vars */
 
-describe('components/Code', () => {});
+describe('components/Code', () => {
+  it('should render inline code in code tag', () => {
+    const component = testHelpers.renderIntoDocument(
+      <Code type="inline">{ 'var foo;' }</Code>
+    );
+
+    const componentNode = testHelpers.getNodeFromComponent(component);
+
+    expect(componentNode.tagName).toBe('CODE');
+  });
+
+  it('should render block code as preformatted text', () => {
+    const component = testHelpers.renderIntoDocument(
+      <Code type="block">{ 'var foo;' }</Code>
+    );
+
+    const componentNode = testHelpers.getNodeFromComponent(component);
+
+    expect(componentNode.tagName).toBe('PRE');
+  });
+
+  it('should render code that is passed in', () => {
+    let code = `var foo;`;
+
+    const componentInline = testHelpers.renderIntoDocument(
+      <Code type="inline">{ code }</Code>
+    );
+
+    const componentBlock = testHelpers.renderIntoDocument(
+      <Code type="block">{ code }</Code>
+    );
+
+    const componentInlineNode = testHelpers.getNodeFromComponent(componentInline);
+    const componentBlockNode = testHelpers.getNodeFromComponent(componentBlock);
+
+    expect(componentInlineNode.textContent).toBe(code);
+    expect(componentBlockNode.textContent).toBe(code);
+  });
+});
