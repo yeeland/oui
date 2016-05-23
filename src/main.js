ebdc13a84eb17b0d2ed7c463410cc5f6be010303
Daniel O'Connor <daniel@danoc.me>
2016-05-23 14:44:52 -0700

    Add more components.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ export { default as ArrowsInline } from './components/ArrowsInline';
 export { default as Attention } from './components/Attention';
 export { default as Button } from './components/Button';
 export { default as Code } from './components/Code';
+export { default as Input } from './components/Input';
+export { default as Label } from './components/Label';
