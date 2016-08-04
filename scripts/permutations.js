098b034a0d57e13b9718857998e97591b5f438fc
Dave Rau <dave.rau@optimizely.com>
2016-08-04 11:06:20 -0700

    rough output working

--- scripts/permutations.js
+++ scripts/permutations.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
+const async = require('async');
 const getPermutations = require('react-component-permutations').default;
 
 const src = fs.readFileSync('./src/components/Button/index.js', 'utf8');
 const options = {};
-console.log(getPermutations);
+// console.log(getPermutations);
 const permutations = getPermutations(src, options);
+// console.log(permutations);
 
-console.log(permutations);
+function generatePermutations() {
+  let output = '';
+  async.eachSeries(
+    ['Button', 'css/bootstrap-responsive.css'],
+    function(filename, cb) {
+      fs.readFile(filename, function(err, content) {
+        if (!err) {
+          output += content;
+        }
+
+        // Calling cb makes it go to the next item.
+        cb(err);
+      });
+    },
+    // Final callback after each item has been iterated over.
+    function(err) {
+      console.log('error!');
+    }
+  );
+  console.log(output);
+}
+
+generatePermutations();
 
 /*
 Button.propTypes = {
