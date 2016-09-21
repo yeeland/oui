ed6184296cf6aab1c4d89b04dcabb33167084008
Daniel O'Connor <daniel@danoc.me>
2016-09-20 20:11:30 -0400

    Fix a few that I missed and bring back babel-jest.

--- docs/PropsTable.js
+++ docs/PropsTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Table from '../src/components/Table';
+import Table from 'components/Table';
 
 const PropsTable = (props) => {
   let componentProps = props.componentProps;
