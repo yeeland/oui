da1d6827172d71fb58076a89a8d575c812a17c6a
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-27 10:51:38 -0700

    Fix warnings

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -11,20 +11,20 @@ import PropTypes from 'prop-types';
 
 const ProgressBar = ({ progress }) => {
   return (
-      <div>
-        <label className="oui-label">Traffic Allocation</label>
-        <div className="lego-progress">
-          <div className="lego-progress__bar" style="width: 35%;" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+    <div>
+      <label className="oui-label">Traffic Allocatio</label>
+      <div className="lego-progress">
+        <div className="lego-progress__bar" style="width: 35%;" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <div className="flex flex--1 push-half--top">
+        <div className="flex flex--1 muted milli">
+          Allocated: 35%
         </div>
-        <div className="flex flex--1 push-half--top">
-          <div className="flex flex--1 muted milli">
-            Allocated: 35%
-          </div>
-          <div className="flex flex--1 muted milli flex-justified--end">
-            Available: 65%
-          </div>
+        <div className="flex flex--1 muted milli flex-justified--end">
+          Available: 65%
         </div>
       </div>
+    </div>
       /* eslint-enable */
     );
 };
