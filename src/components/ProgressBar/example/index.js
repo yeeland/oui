5c4752ad887ffea9e9a01511ba375769eec99d8a
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-27 15:15:53 -0700

    ProgressBar ready for Pull Request

--- src/components/ProgressBar/example/index.js
+++ src/components/ProgressBar/example/index.js
@@ -5,7 +5,54 @@ import ProgressBar from '../index';
 export default [
   {
     examples: [
-      <ProgressBar />,
+      <ProgressBar
+        max="100"
+        min="0"
+        progress="60"
+        topLabel="hola esto es un label"
+      />,
+    ],
+  },
+    {
+    examples: [
+      <ProgressBar
+        max="100"
+        min="0"
+        progress="60"
+      />,
+    ],
+  },
+  {
+    examples: [
+      <ProgressBar
+        leftLabel="Allocated traffic"
+        max="100"
+        min="0"
+        progress="60"
+        rightLabel="Available traffic"
+      />,
+    ],
+  },
+  {
+    examples: [
+      <ProgressBar
+        leftLabel="Allocated traffic"
+        min="0"
+        max="100"
+        progress="60"
+        rightLabel="Available traffic"
+        topLabel="hola esto es un label"
+      />,
+    ],
+  },
+  {
+    examples: [
+      <ProgressBar
+        displayError
+        min="0"
+        max="100"
+        progress="60"
+      />,
     ],
   },
 ];
