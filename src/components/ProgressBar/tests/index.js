3f8d31038d4b586c0956de28a0625e41eaa50a2f
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-27 16:15:07 -0700

    One test

--- src/components/ProgressBar/tests/index.js
+++ src/components/ProgressBar/tests/index.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import ProgressBar from '../index';
+import Label from '../../Label';
+import { shallow } from 'enzyme';
+
 describe('components/ProgressBar', () => {
-  it('testing ProgressBar', () => {
-    expect(true).toBe(true);
+  it('should render a progressbar with a top label', () => {
+    const wrapper = shallow(
+      <ProgressBar topLabel="this is a top label" />
+    );
+    expect(wrapper.prop('topLable')).toEqual('this is a top label');
   });
 });
