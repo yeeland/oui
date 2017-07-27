76a26e1fb29b1953d3a823a111d409728e1ab79b
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-27 10:22:54 -0700

    add ProgressBar export to main.js

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,44 +1,141 @@
 import React from 'react';
+import Label from '../Label';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import OverlayWrapper from '../OverlayWrapper';
-import Poptip from './Poptip';
 
 /**
- * Displays help text when hovering on an element.
+ * Generates an `input` element (optionally wrapped in a label) and accepts
+ * most of the common input types.
  * @param {Object} props - Properties passed to component
  * @returns {ReactElement}
  */
-const ProgressBar = (props) => (
-  <OverlayWrapper
-    behavior="hover"
-    overlay={ <Poptip testSection={ props.testSection }>
-      { props.content }
-    </Poptip> }
-    horizontalAttachment={ props.horizontalAttachment }
-    horizontalTargetAttachment={ props.horizontalTargetAttachment }
-    verticalAttachment={ props.verticalAttachment }
-    verticalTargetAttachment={ props.verticalTargetAttachment }>
-    { props.children }
-  </OverlayWrapper>
-);
 
-ProgressBar.displayName = 'Poptip';
+class ProgressBar extends React.Component {
+
+  blur() {
+    if (this._input) {
+      this._input.blur();
+    }
+  }
+
+  renderInput(opts) {
+    let noteLabel = null;
+    if (opts.note) {
+      noteLabel = <div className="oui-form-note">{ opts.note }</div>;
+    }
+
+    let wrapperClasses = classnames(
+      {'oui-form-bad-news': opts.displayError}
+    );
+
+    let classes = classnames(
+      'oui-text-input', {'oui-text-input--search': opts.isFilter}
+    );
+
+    return (
+      /* eslint-disable react/jsx-no-bind */
+      <div data-oui-component={ true } className={ wrapperClasses }>
+        <Label
+          displayError={ opts.displayError }
+          isRequired={ opts.isRequired }
+          isOptional={ opts.isOptional }>
+          { opts.label }
+        </Label>
+        <input
+          className={ classes }
+          ref={ (c) => { this._input = c; } }
+          type={ opts.type }
+          value={ opts.value }
+          defaultValue={ opts.defaultValue }
+          placeholder={ opts.placeholder }
+          required={ opts.isRequired }
+          readOnly={ opts.isReadOnly }
+          disabled={ opts.isDisabled }
+          onInput={ opts.onInput }
+          onChange={ opts.onChange }
+          onBlur={ opts.onBlur }
+          onKeyDown={ opts.onKeyDown }
+          onFocus={ opts.onFocus }
+          min={ opts.min }
+          max={ opts.max }
+          data-test-section={ opts.testSection }
+        />
+        { noteLabel }
+      </div>
+      /* eslint-enable */
+    );
+  }
+
+  render() {
+    return this.renderInput(this.props);
+  }
+}
 
 ProgressBar.propTypes = {
-  /** Content that, when hovered on, makes the Poptip appear */
-  children: PropTypes.node.isRequired,
-  /** Text that appears within the poptip */
-  content: PropTypes.string.isRequired,
-  /** Side of the poptip that should attach to the `children` */
-  horizontalAttachment: PropTypes.oneOf(['left', 'center', 'right']),
-  /** Side of `children` that should attach to the poptip */
-  horizontalTargetAttachment: PropTypes.oneOf(['left', 'center', 'right']),
+  /** The default value of the input used on initial render */
+  defaultValue: PropTypes.string,
+  /** Toggle error state styles  */
+  displayError: PropTypes.bool,
+  /** Prevents input from being modified and appears disabled */
+  isDisabled: PropTypes.bool,
+  /** Includes error if true */
+  isFilter: PropTypes.bool,
+  /** Adds an optional label if there is a label provided
+   *  @param {Object} props Object of props
+   *  @returns {Error} Error or null
+   */
+  isOptional: function verifyIsOptionalProp(props) {
+    if (props.isOptional && !props.label) {
+      return new Error('Must include a value for the label prop to use the isOptional prop');
+    }
+    return null;
+  },
+  /** Prevents input from being modified but doesn't appear disabled */
+  isReadOnly: PropTypes.bool,
+  /** Prevents input from being submitted without value */
+  isRequired: PropTypes.bool,
+  /** Text that describes the input */
+  label: PropTypes.string,
+  /**
+   * Max value for the `input`. Should be used only when `type` is `number`.
+   */
+  max: PropTypes.number,
+  /**
+   * Min value for the `input`. Should be used only when `type` is `number`.
+   */
+  min: PropTypes.number,
+  /** Append note near form input. */
+  note: PropTypes.string,
+  /**
+   * Function that fires when the input loses focus. It fires regardless of
+   * whether the value has changed.
+  */
+  onBlur: PropTypes.func,
+  /** Function that fires when the input loses focus after the value changes */
+  onChange: PropTypes.func,
+  /** Function that fires when the input gains focus */
+  onFocus: PropTypes.func,
+  /** Function that fires on keypress */
+  onInput: PropTypes.func,
+  /** Function that fires when a key is pressed down */
+  onKeyDown: PropTypes.func,
+  /** Input placeholder text */
+  placeholder: PropTypes.string,
   /** Hook for automated JavaScript tests */
   testSection: PropTypes.string,
-  /** Vertical edge of the poptip that should touch the `children` */
-  verticalAttachment: PropTypes.oneOf(['top', 'middle', 'bottom']),
-  /** Vertical edge of the `children` that should touch the poptip */
-  verticalTargetAttachment: PropTypes.oneOf(['top', 'middle', 'bottom']),
+  /** Supported input types */
+  type: PropTypes.oneOf([
+    'text',
+    'password',
+    'date',
+    'number',
+    'email',
+    'url',
+    'search',
+    'tel',
+  ]).isRequired,
+  /** Text within the input */
+  value: PropTypes.string,
 };
 
 export default ProgressBar;
