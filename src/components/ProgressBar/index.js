9e5e6a710400884dd90d431580c6259fa9e36560
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2017-07-27 09:54:17 -0700

    Add ProgressBar base code

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import OverlayWrapper from '../OverlayWrapper';
+import Poptip from './Poptip';
+
+/**
+ * Displays help text when hovering on an element.
+ * @param {Object} props - Properties passed to component
+ * @returns {ReactElement}
+ */
+const ProgressBar = (props) => (
+  <OverlayWrapper
+    behavior="hover"
+    overlay={ <Poptip testSection={ props.testSection }>
+      { props.content }
+    </Poptip> }
+    horizontalAttachment={ props.horizontalAttachment }
+    horizontalTargetAttachment={ props.horizontalTargetAttachment }
+    verticalAttachment={ props.verticalAttachment }
+    verticalTargetAttachment={ props.verticalTargetAttachment }>
+    { props.children }
+  </OverlayWrapper>
+);
+
+ProgressBar.displayName = 'Poptip';
+
+ProgressBar.propTypes = {
+  /** Content that, when hovered on, makes the Poptip appear */
+  children: PropTypes.node.isRequired,
+  /** Text that appears within the poptip */
+  content: PropTypes.string.isRequired,
+  /** Side of the poptip that should attach to the `children` */
+  horizontalAttachment: PropTypes.oneOf(['left', 'center', 'right']),
+  /** Side of `children` that should attach to the poptip */
+  horizontalTargetAttachment: PropTypes.oneOf(['left', 'center', 'right']),
+  /** Hook for automated JavaScript tests */
+  testSection: PropTypes.string,
+  /** Vertical edge of the poptip that should touch the `children` */
+  verticalAttachment: PropTypes.oneOf(['top', 'middle', 'bottom']),
+  /** Vertical edge of the `children` that should touch the poptip */
+  verticalTargetAttachment: PropTypes.oneOf(['top', 'middle', 'bottom']),
+};
+
+export default ProgressBar;
